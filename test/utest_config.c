36a59134fe1ae4e9aec180834420557c36f39193
Ales Teska <ales.teska@teskalabs.com>
2016-03-31 15:42:52 +0100

    Fixing broken unit test.

--- test/utest_config.c
+++ test/utest_config.c
@@ -6,8 +6,9 @@
 START_TEST(config_init_core_utest)
 {
 	ck_assert_int_eq(libsccmn_config.initialized, false);
-	libsccmn_config_init();
+	libsccmn_initialize();
 	ck_assert_int_eq(libsccmn_config.initialized, true);
+	libsccmn_configure();
 }
 END_TEST
 
