8a66a7b4b97a49b28bea0778dbdbb808290e75b9
Ales Teska <ales.teska@teskalabs.com>
2016-06-25 14:04:16 +0200

    Improving a reply test.

--- examples/reply/test_reply_01.sh
+++ examples/reply/test_reply_01.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-dd if=/dev/zero count=1024 bs=1024 > /tmp/sc-test-reply-01.bin
+dd if=/dev/urandom count=1024 bs=1024 > /tmp/sc-test-reply-01.bin
 md5 /tmp/sc-test-reply-01.bin
 
-cat /tmp/sc-test-reply-01.bin | nc -D -U /tmp/sctext.tmp | md5
+cat /tmp/sc-test-reply-01.bin | pv -r | nc -U /tmp/sctext.tmp | pv -r | md5
+#cat /tmp/sc-test-reply-01.bin | pv -r | nc 127.0.0.1 12345  | pv -r | md5
