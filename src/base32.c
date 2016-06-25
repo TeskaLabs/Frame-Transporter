25658b1eaa8db075a7be1aa54c2ae3593720ca9f
Ales Teska <ales.teska@teskalabs.com>
2016-06-25 18:56:57 +0200

    Spaces to tabs.

--- src/base32.c
+++ src/base32.c
@@ -26,79 +26,79 @@ static const char base32_cb32[] =  "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 
 int base32_encode(char * buf, size_t * buflen, const void * data, size_t size)
 {
-    unsigned char *udata = (unsigned char *) data;
-    int iout = 0;   /* to-be-filled output char */
-    int iin = 0;    /* one more than last input byte that can be
-               successfully decoded */
+	unsigned char *udata = (unsigned char *) data;
+	int iout = 0;   /* to-be-filled output char */
+	int iin = 0;    /* one more than last input byte that can be
+			   successfully decoded */
 
-    /* Note: Don't bother to optimize manually. GCC optimizes
-       better(!) when using simplistic array indexing. */
+	/* Note: Don't bother to optimize manually. GCC optimizes
+	   better(!) when using simplistic array indexing. */
 
-    while (1) {
-        if (iout >= *buflen || iin >= size)
-            break;
-        buf[iout] = base32_cb32[((udata[iin] & 0xf8) >> 3)];
-        iout++;
+	while (1) {
+		if (iout >= *buflen || iin >= size)
+			break;
+		buf[iout] = base32_cb32[((udata[iin] & 0xf8) >> 3)];
+		iout++;
 
-        if (iout >= *buflen || iin >= size) {
-            iout--;     /* previous char is useless */
-            break;
-        }
-        buf[iout] = base32_cb32[((udata[iin] & 0x07) << 2) |
-                  ((iin + 1 < size) ?
-                   ((udata[iin + 1] & 0xc0) >> 6) : 0)];
-        iin++;          /* 0 complete, iin=1 */
-        iout++;
+		if (iout >= *buflen || iin >= size) {
+			iout--;     /* previous char is useless */
+			break;
+		}
+		buf[iout] = base32_cb32[((udata[iin] & 0x07) << 2) |
+				  ((iin + 1 < size) ?
+				   ((udata[iin + 1] & 0xc0) >> 6) : 0)];
+		iin++;          /* 0 complete, iin=1 */
+		iout++;
 
-        if (iout >= *buflen || iin >= size)
-            break;
-        buf[iout] = base32_cb32[((udata[iin] & 0x3e) >> 1)];
-        iout++;
+		if (iout >= *buflen || iin >= size)
+			break;
+		buf[iout] = base32_cb32[((udata[iin] & 0x3e) >> 1)];
+		iout++;
 
-        if (iout >= *buflen || iin >= size) {
-            iout--;     /* previous char is useless */
-            break;
-        }
-        buf[iout] = base32_cb32[((udata[iin] & 0x01) << 4) |
-                  ((iin + 1 < size) ?
-                   ((udata[iin + 1] & 0xf0) >> 4) : 0)];
-        iin++;          /* 1 complete, iin=2 */
-        iout++;
+		if (iout >= *buflen || iin >= size) {
+			iout--;     /* previous char is useless */
+			break;
+		}
+		buf[iout] = base32_cb32[((udata[iin] & 0x01) << 4) |
+				  ((iin + 1 < size) ?
+				   ((udata[iin + 1] & 0xf0) >> 4) : 0)];
+		iin++;          /* 1 complete, iin=2 */
+		iout++;
 
-        if (iout >= *buflen || iin >= size)
-            break;
-        buf[iout] = base32_cb32[((udata[iin] & 0x0f) << 1) |
-                  ((iin + 1 < size) ?
-                   ((udata[iin + 1] & 0x80) >> 7) : 0)];
-        iin++;          /* 2 complete, iin=3 */
-        iout++;
+		if (iout >= *buflen || iin >= size)
+			break;
+		buf[iout] = base32_cb32[((udata[iin] & 0x0f) << 1) |
+				  ((iin + 1 < size) ?
+				   ((udata[iin + 1] & 0x80) >> 7) : 0)];
+		iin++;          /* 2 complete, iin=3 */
+		iout++;
 
-        if (iout >= *buflen || iin >= size)
-            break;
-        buf[iout] = base32_cb32[((udata[iin] & 0x7c) >> 2)];
-        iout++;
+		if (iout >= *buflen || iin >= size)
+			break;
+		buf[iout] = base32_cb32[((udata[iin] & 0x7c) >> 2)];
+		iout++;
 
-        if (iout >= *buflen || iin >= size) {
-            iout--;     /* previous char is useless */
-            break;
-        }
-        buf[iout] = base32_cb32[((udata[iin] & 0x03) << 3) |
-                  ((iin + 1 < size) ?
-                   ((udata[iin + 1] & 0xe0) >> 5) : 0)];
-        iin++;          /* 3 complete, iin=4 */
-        iout++;
+		if (iout >= *buflen || iin >= size) {
+			iout--;     /* previous char is useless */
+			break;
+		}
+		buf[iout] = base32_cb32[((udata[iin] & 0x03) << 3) |
+				  ((iin + 1 < size) ?
+				   ((udata[iin + 1] & 0xe0) >> 5) : 0)];
+		iin++;          /* 3 complete, iin=4 */
+		iout++;
 
-        if (iout >= *buflen || iin >= size)
-            break;
-        buf[iout] = base32_cb32[((udata[iin] & 0x1f))];
-        iin++;          /* 4 complete, iin=5 */
-        iout++;
-    }
+		if (iout >= *buflen || iin >= size)
+			break;
+		buf[iout] = base32_cb32[((udata[iin] & 0x1f))];
+		iin++;          /* 4 complete, iin=5 */
+		iout++;
+	}
 
-    buf[iout] = '\0';
+	buf[iout] = '\0';
 
-    /* store number of bytes from data that was used */
-    *buflen = iin;
+	/* store number of bytes from data that was used */
+	*buflen = iin;
 
-    return iout;
+	return iout;
 }
