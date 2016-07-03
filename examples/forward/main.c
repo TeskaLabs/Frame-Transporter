d16bfbaf996505f692a17c2065a638314a134034
Ales Teska <ales.teska@teskalabs.com>
2016-07-03 18:29:56 +0200

    Removing trace log.

--- examples/forward/main.c
+++ examples/forward/main.c
@@ -239,8 +239,7 @@ int main(int argc, char const *argv[])
 	logging_set_verbose(true);
 	libsccmn_init();
 	
-	libsccmn_config.log_verbose = true;
-	libsccmn_config.log_trace_mask |= L_TRACEID_SOCK_STREAM | L_TRACEID_EVENT_LOOP;
+	//libsccmn_config.log_trace_mask |= L_TRACEID_SOCK_STREAM | L_TRACEID_EVENT_LOOP;
 
 	// Initializa context
 	ok = context_init(&context);
