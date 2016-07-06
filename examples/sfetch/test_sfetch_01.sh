8b2e197b95634f9334750f76304879bff04431e2
Ales Teska <ales.teska@teskalabs.com>
2016-07-06 23:11:44 +0200

    Improving sfetch test.

--- examples/sfetch/test_sfetch_01.sh
+++ examples/sfetch/test_sfetch_01.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -e
 
 while read p; do
-	echo "Testing", ${p}
 	read -ra arr <<< "${p}"
-	./sfetch ${arr[0]} ${arr[1]}
+	./sfetch ${arr[0]} ${arr[1]} > /dev/null
+	if [ $? -ne 0 ]; then
+		echo ${p} "FAILED"
+	else
+		echo ${p} "OK"
+	fi
 done < ssl_sites.txt
 
 echo "TEST $0 OK"
