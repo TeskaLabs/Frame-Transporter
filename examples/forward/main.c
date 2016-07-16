7519fa31f4912a04500351e97a142dd076dcf12c
Ales Teska <ales.teska@teskalabs.com>
2016-07-16 21:47:52 +0100

    Updating examples with latest changes.

--- examples/forward/main.c
+++ examples/forward/main.c
@@ -8,22 +8,24 @@
 struct exiting_watcher watcher;
 struct addrinfo * target_addr;
 
+struct ft_list listeners;
+
 ///
 
-struct est_sock_pair
+struct stream_pair
 {
-	struct established_socket in_sock;
-	struct established_socket out_sock;
+	struct established_socket stream_in;
+	struct established_socket stream_out;
 };
 
-struct ft_list established_sock_pairs; // List of established_socket(s) pairs
+struct ft_list stream_pairs; // List of established_socket(s) pairs
 
 
 bool on_read_in_to_out(struct established_socket * established_sock, struct frame * frame)
 {
 	frame_flip(frame);
-	struct est_sock_pair * pair = (struct est_sock_pair *)established_sock->data;
-	bool ok = established_socket_write(&pair->out_sock, frame);
+	struct stream_pair * pair = (struct stream_pair *)established_sock->data;
+	bool ok = established_socket_write(&pair->stream_out, frame);
 	if (!ok)
 	{
 		L_ERROR_P("Cannot write a frame!");
@@ -36,8 +38,8 @@ bool on_read_in_to_out(struct established_socket * established_sock, struct fram
 bool on_read_out_to_in(struct established_socket * established_sock, struct frame * frame)
 {
 	frame_flip(frame);
-	struct est_sock_pair * pair = (struct est_sock_pair *)established_sock->data;
-	bool ok = established_socket_write(&pair->in_sock, frame);
+	struct stream_pair * pair = (struct stream_pair *)established_sock->data;
+	bool ok = established_socket_write(&pair->stream_in, frame);
 	if (!ok)
 	{
 		L_ERROR_P("Cannot write a frame!");
@@ -49,20 +51,20 @@ bool on_read_out_to_in(struct established_socket * established_sock, struct fram
 
 void on_connected(struct established_socket * established_sock)
 {
-	struct est_sock_pair * pair = (struct est_sock_pair *)established_sock->data;
-	established_socket_read_start(&pair->in_sock);
-	established_socket_read_start(&pair->out_sock);
+	struct stream_pair * pair = (struct stream_pair *)established_sock->data;
+	established_socket_read_start(&pair->stream_in);
+	established_socket_read_start(&pair->stream_out);
 }
 
 void on_error(struct established_socket * established_sock)
 {
-	struct est_sock_pair * pair = (struct est_sock_pair *)established_sock->data;
+	struct stream_pair * pair = (struct stream_pair *)established_sock->data;
 	//TODO: maybe even hard shutdown
-	established_socket_write_shutdown(&pair->in_sock);
-	established_socket_write_shutdown(&pair->out_sock);
+	established_socket_write_shutdown(&pair->stream_in);
+	established_socket_write_shutdown(&pair->stream_out);
 }
 
-struct established_socket_cb sock_est_in_sock_cb = 
+struct established_socket_cb sock_est_stream_in_cb = 
 {
 	.get_read_frame = established_socket_get_read_frame_simple,
 	.read = on_read_in_to_out,
@@ -70,7 +72,7 @@ struct established_socket_cb sock_est_in_sock_cb =
 };
 
 
-struct established_socket_cb sock_est_out_sock_cb = 
+struct established_socket_cb sock_est_stream_out_cb = 
 {
 	.get_read_frame = established_socket_get_read_frame_simple,
 	.read = on_read_out_to_in,
@@ -78,73 +80,62 @@ struct established_socket_cb sock_est_out_sock_cb =
 	.error = on_error,
 };
 
-static void established_sock_pair_stop_each(struct ft_list_node * node, void * data)
+static void stream_pairs_on_remove(struct ft_list * list, struct ft_node * node)
 {
-	struct est_sock_pair * pair = (struct est_sock_pair *)&node->payload;
-	established_socket_read_stop(&pair->in_sock);
-	established_socket_write_stop(&pair->in_sock);
-	established_socket_read_stop(&pair->out_sock);
-	established_socket_write_stop(&pair->out_sock);
-}
-
-static void established_sock_pair_node_fini_cb(struct ft_list_node * node)
-{
-	struct est_sock_pair * pair = (struct est_sock_pair *)&node->payload;
+	struct stream_pair * pair = (struct stream_pair *)&node->data;
 
 	L_INFO("Stats  IN: Re:%u We:%u+%u Rb:%lu Wb:%lu",
-		pair->in_sock.stats.read_events,
-		pair->in_sock.stats.write_events,
-		pair->in_sock.stats.write_direct,
-		pair->in_sock.stats.read_bytes,
-		pair->in_sock.stats.write_bytes
+		pair->stream_in.stats.read_events,
+		pair->stream_in.stats.write_events,
+		pair->stream_in.stats.write_direct,
+		pair->stream_in.stats.read_bytes,
+		pair->stream_in.stats.write_bytes
 	);
 
 	L_INFO("Stats OUT: Re:%u We:%u+%u Rb:%lu Wb:%lu",
-		pair->out_sock.stats.read_events,
-		pair->out_sock.stats.write_events,
-		pair->out_sock.stats.write_direct,
-		pair->out_sock.stats.read_bytes,
-		pair->out_sock.stats.write_bytes
+		pair->stream_out.stats.read_events,
+		pair->stream_out.stats.write_events,
+		pair->stream_out.stats.write_direct,
+		pair->stream_out.stats.read_bytes,
+		pair->stream_out.stats.write_bytes
 	);
 
-	established_socket_fini(&pair->in_sock);
-	established_socket_fini(&pair->out_sock);
+	established_socket_fini(&pair->stream_in);
+	established_socket_fini(&pair->stream_out);
 }
 
 ///
-
-struct ft_list listen_socks; // List of listening_socket(s)
 
 static bool on_accept_cb(struct listening_socket * listening_socket, int fd, const struct sockaddr * client_addr, socklen_t client_addr_len)
 {
 	bool ok;
 
-	struct ft_list_node * new_node = ft_list_node_new(sizeof(struct est_sock_pair));
+	struct ft_node * new_node = ft_node_new(sizeof(struct stream_pair));
 	if (new_node == NULL) return false;
 
-	struct est_sock_pair * pair = (struct est_sock_pair *)&new_node->payload;
+	struct stream_pair * pair = (struct stream_pair *)&new_node->data;
 
 	// Initialize connection on the incoming connection
-	ok = established_socket_init_accept(&pair->in_sock, &sock_est_in_sock_cb, listening_socket, fd, client_addr, client_addr_len);
+	ok = established_socket_init_accept(&pair->stream_in, &sock_est_stream_in_cb, listening_socket, fd, client_addr, client_addr_len);
 	if (!ok)
 	{
-		ft_list_node_del(new_node, &established_sock_pairs);
+		ft_node_del(new_node);
 		return false;
 	}
-	pair->in_sock.data = pair;
-	established_socket_set_read_partial(&pair->in_sock, true);
+	pair->stream_in.data = pair;
+	established_socket_set_read_partial(&pair->stream_in, true);
 
 	// Initialize connection on the outgoing connection
-	ok = established_socket_init_connect(&pair->out_sock, &sock_est_out_sock_cb, listening_socket->context, target_addr);
+	ok = established_socket_init_connect(&pair->stream_out, &sock_est_stream_out_cb, listening_socket->context, target_addr);
 	if (!ok)
 	{
-		ft_list_node_del(new_node, &established_sock_pairs);
+		ft_node_del(new_node);
 		return false;
 	}
-	pair->out_sock.data = pair;
-	established_socket_set_read_partial(&pair->out_sock, true);
+	pair->stream_out.data = pair;
+	established_socket_set_read_partial(&pair->stream_out, true);
 
-	ft_list_add(&established_sock_pairs, new_node);
+	ft_list_add(&stream_pairs, new_node);
 
 	return true;
 }
@@ -156,64 +147,69 @@ struct listening_socket_cb sock_listen_sock_cb =
 
 static bool on_resolve_listen_cb(void * data, struct context * context, struct addrinfo * addrinfo)
 {
-	struct ft_list_node * new_node = ft_list_node_new(sizeof(struct listening_socket));
+	struct ft_node * new_node = ft_node_new(sizeof(struct listening_socket));
 	if (new_node == NULL) return false;
 
-	bool ok = listening_socket_init((struct listening_socket *)new_node->payload, &sock_listen_sock_cb, context, addrinfo);
+	struct listening_socket * listener = (struct listening_socket *)new_node->data;
+
+	bool ok = listening_socket_init(listener, &sock_listen_sock_cb, context, addrinfo);
 	if (!ok) 
 	{
-		ft_list_node_del(new_node, &listen_socks);
+		ft_node_del(new_node);
 		return false;
 	}
 
-	ft_list_add(&listen_socks, new_node);
+	ft_list_add(&listeners, new_node);
 
 	return true;
 }
 
-static void listen_sock_node_fini_cb(struct ft_list_node * node)
+static void listeners_on_remove(struct ft_list * list, struct ft_node * node)
 {
-	listening_socket_fini((struct listening_socket *)node->payload);
+	listening_socket_fini((struct listening_socket *)node->data);
 }
 
-static void listen_sock_start_each(struct ft_list_node * node, void * data)
+static void listen_sock_start_each(struct ft_node * node, void * data)
 {
-	listening_socket_start((struct listening_socket *)node->payload);
-}
-
-static void listen_sock_stop_each(struct ft_list_node * node, void * data)
-{
-	listening_socket_stop((struct listening_socket *)node->payload);
+	listening_socket_start((struct listening_socket *)node->data);
 }
 
 ///
 
+static void stream_pairs_stop_each(struct ft_node * node, void * data)
+{
+	struct stream_pair * pair = (struct stream_pair *)&node->data;
+	established_socket_read_stop(&pair->stream_in);
+	established_socket_write_stop(&pair->stream_in);
+	established_socket_read_stop(&pair->stream_out);
+	established_socket_write_stop(&pair->stream_out);
+}
+
+static void listeners_stop_each(struct ft_node * node, void * data)
+{
+	listening_socket_stop((struct listening_socket *)node->data);
+}
+
 static void on_exiting_cb(struct exiting_watcher * watcher, struct context * context)
 {
-	// Stop established sockets
-	ft_list_each(&established_sock_pairs, established_sock_pair_stop_each, NULL);
-
-	// Stop listening sockets
-	ft_list_each(&listen_socks, listen_sock_stop_each, NULL);
+	ft_list_each(&stream_pairs, stream_pairs_stop_each, NULL);
+	ft_list_each(&listeners, listeners_stop_each, NULL);
 }
+
+///
 
 static void on_check_cb(struct ev_loop * loop, ev_prepare * check, int revents)
 {
 restart:
-	for (struct ft_list_node * node = established_sock_pairs.head; node != NULL; node = node->next)
+	for (struct ft_node * node = stream_pairs.head; node != NULL; node = node->right)
 	{
-		struct est_sock_pair * pair = (struct est_sock_pair *)&node->payload;
-		if (established_socket_is_shutdown(&pair->in_sock) || established_socket_is_shutdown(&pair->out_sock))
+		struct stream_pair * pair = (struct stream_pair *)&node->data;
+		if (established_socket_is_shutdown(&pair->stream_in) || established_socket_is_shutdown(&pair->stream_out))
 		{
-			// L_DEBUG("Removing socket pair: %c %c",
-			// 	established_socket_is_shutdown(&pair->in_sock) ? '-' : 'O',
-			// 	established_socket_is_shutdown(&pair->out_sock) ? '-' : 'O'
-			// );
-			ft_list_remove(&established_sock_pairs, node);
+			ft_list_remove(&stream_pairs, node);
 			goto restart;
 		}
 	}
-
 }
 
 ///
@@ -261,13 +257,8 @@ int main(int argc, char const *argv[])
 	ok = context_init(&context);
 	if (!ok) return EXIT_FAILURE;
 
-	// Initialize a list for listening sockets
-	ok = ft_list_init(&listen_socks, listen_sock_node_fini_cb);
-	if (!ok) return EXIT_FAILURE;
-
-	// Initialize a list for established socket pairs
-	ok = ft_list_init(&established_sock_pairs, established_sock_pair_node_fini_cb);
-	if (!ok) return EXIT_FAILURE;
+	ft_list_init(&listeners, listeners_on_remove);
+	ft_list_init(&stream_pairs, stream_pairs_on_remove);
 
 	// Prepare listening socket(s)
 	rc = listening_socket_create_getaddrinfo(on_resolve_listen_cb, NULL, &context, AF_INET, SOCK_STREAM, "127.0.0.1", "12345");
@@ -300,7 +291,7 @@ int main(int argc, char const *argv[])
 	}
 
 	// Start listening
-	ft_list_each(&listen_socks, listen_sock_start_each, NULL);
+	ft_list_each(&listeners, listen_sock_start_each, NULL);
 
 	// Register exiting watcher
 	context_exiting_watcher_add(&context, &watcher, on_exiting_cb);
@@ -308,13 +299,9 @@ int main(int argc, char const *argv[])
 	// Enter event loop
 	context_evloop_run(&context);
 
-	// Remove established sockets
-	ft_list_fini(&established_sock_pairs);
-
-	// Remove listening sockets
-	ft_list_fini(&listen_socks);
-
-	// Finalize context
+	// Clean up
+	ft_list_fini(&stream_pairs);
+	ft_list_fini(&listeners);
 	context_fini(&context);
 
 	return EXIT_SUCCESS;
--- examples/sreply/main.c
+++ examples/sreply/main.c
@@ -9,9 +9,10 @@ SSL_CTX * ssl_ctx;
 struct context context;
 struct exiting_watcher watcher;
 
-///
+struct ft_list streams;
+struct ft_list listeners;
 
-struct ft_list established_socks; // List of established_socket(s)
+///
 
 bool on_read(struct established_socket * established_sock, struct frame * frame)
 {
@@ -34,44 +35,43 @@ struct established_socket_cb sock_est_sock_cb =
 	.error = NULL,
 };
 
-static void established_sock_stop_each(struct ft_list_node * node, void * data)
+static void established_sock_stop_each(struct ft_node * node, void * data)
 {
-	established_socket_read_stop((struct established_socket *)node->payload);
-	established_socket_write_stop((struct established_socket *)node->payload);
+	struct established_socket * stream = (struct established_socket *)node->data;
+	established_socket_read_stop(stream);
+	established_socket_write_stop(stream);
 }
 
-static void established_sock_node_fini_cb(struct ft_list_node * node)
+static void streams_on_remove(struct ft_list * list, struct ft_node * node)
 {
-	struct established_socket * sock = (struct established_socket *)node->payload;
+	struct established_socket * stream = (struct established_socket *)node->data;
 
 	L_INFO("Stats: Re:%u We:%u+%u Rb:%lu Wb:%lu",
-		sock->stats.read_events,
-		sock->stats.write_events,
-		sock->stats.write_direct,
-		sock->stats.read_bytes,
-		sock->stats.write_bytes
+		stream->stats.read_events,
+		stream->stats.write_events,
+		stream->stats.write_direct,
+		stream->stats.read_bytes,
+		stream->stats.write_bytes
 	);
 
-	established_socket_fini(sock);
+	established_socket_fini(stream);
 }
 
 ///
-
-struct ft_list listen_socks; // List of listening_socket(s)
 
 static bool on_accept_cb(struct listening_socket * listening_socket, int fd, const struct sockaddr * client_addr, socklen_t client_addr_len)
 {
 	bool ok;
 
-	struct ft_list_node * new_node = ft_list_node_new(sizeof(struct established_socket));
+	struct ft_node * new_node = ft_node_new(sizeof(struct established_socket));
 	if (new_node == NULL) return false;
 
-	struct established_socket * established_sock = (struct established_socket *)&new_node->payload;
+	struct established_socket * established_sock = (struct established_socket *)&new_node->data;
 
 	ok = established_socket_init_accept(established_sock, &sock_est_sock_cb, listening_socket, fd, client_addr, client_addr_len);
 	if (!ok)
 	{
-		ft_list_node_del(new_node, &established_socks);
+		ft_node_del(new_node);
 		return false;
 	}
 
@@ -82,7 +82,7 @@ static bool on_accept_cb(struct listening_socket * listening_socket, int fd, con
 	if (!ok) return EXIT_FAILURE;
 
 
-	ft_list_add(&established_socks, new_node);
+	ft_list_add(&streams, new_node);
 
 	return true;
 }
@@ -94,34 +94,34 @@ struct listening_socket_cb sock_listen_sock_cb =
 
 static bool on_resolve_listen_cb(void * data, struct context * context, struct addrinfo * addrinfo)
 {
-	struct ft_list_node * new_node = ft_list_node_new(sizeof(struct listening_socket));
+	struct ft_node * new_node = ft_node_new(sizeof(struct listening_socket));
 	if (new_node == NULL) return false;
 
-	bool ok = listening_socket_init((struct listening_socket *)new_node->payload, &sock_listen_sock_cb, context, addrinfo);
+	bool ok = listening_socket_init((struct listening_socket *)new_node->data, &sock_listen_sock_cb, context, addrinfo);
 	if (!ok) 
 	{
-		ft_list_node_del(new_node, &listen_socks);
+		ft_node_del(new_node);
 		return false;
 	}
 
-	ft_list_add(&listen_socks, new_node);
+	ft_list_add(&listeners, new_node);
 
 	return true;
 }
 
-static void listen_sock_node_fini_cb(struct ft_list_node * node)
+static void listeners_on_remove(struct ft_list * list, struct ft_node * node)
 {
-	listening_socket_fini((struct listening_socket *)node->payload);
+	listening_socket_fini((struct listening_socket *)node->data);
 }
 
-static void listen_sock_start_each(struct ft_list_node * node, void * data)
+static void listen_sock_start_each(struct ft_node * node, void * data)
 {
-	listening_socket_start((struct listening_socket *)node->payload);
+	listening_socket_start((struct listening_socket *)node->data);
 }
 
-static void listen_sock_stop_each(struct ft_list_node * node, void * data)
+static void listen_sock_stop_each(struct ft_node * node, void * data)
 {
-	listening_socket_stop((struct listening_socket *)node->payload);
+	listening_socket_stop((struct listening_socket *)node->data);
 }
 
 ///
@@ -129,10 +129,10 @@ static void listen_sock_stop_each(struct ft_list_node * node, void * data)
 static void on_exiting_cb(struct exiting_watcher * watcher, struct context * context)
 {
 	// Stop established sockets
-	ft_list_each(&established_socks, established_sock_stop_each, NULL);
+	ft_list_each(&streams, established_sock_stop_each, NULL);
 
 	// Stop listening sockets
-	ft_list_each(&listen_socks, listen_sock_stop_each, NULL);
+	ft_list_each(&listeners, listen_sock_stop_each, NULL);
 }
 
 static void on_check_cb(struct ev_loop * loop, ev_prepare * check, int revents)
@@ -148,13 +148,13 @@ static void on_check_cb(struct ev_loop * loop, ev_prepare * check, int revents)
 
 	// Check all established socket and remove closed ones
 restart:
-	for (struct ft_list_node * node = established_socks.head; node != NULL; node = node->next)
+	for (struct ft_node * node = streams.head; node != NULL; node = node->right)
 	{
-		struct established_socket * sock = (struct established_socket *)node->payload;
+		struct established_socket * sock = (struct established_socket *)node->data;
 
 		if (established_socket_is_shutdown(sock))
 		{
-			ft_list_remove(&established_socks, node);
+			ft_list_remove(&streams, node);
 			goto restart;
 		}
 
@@ -193,13 +193,8 @@ int main(int argc, char const *argv[])
 	rc = SSL_CTX_use_certificate_file(ssl_ctx, "./cert.pem", SSL_FILETYPE_PEM);
 	if (rc != 1) return EXIT_FAILURE;
 
-	// Initialize a list for listening sockets
-	ok = ft_list_init(&listen_socks, listen_sock_node_fini_cb);
-	if (!ok) return EXIT_FAILURE;
-
-	// Initialize a list for established sockets
-	ok = ft_list_init(&established_socks, established_sock_node_fini_cb);
-	if (!ok) return EXIT_FAILURE;
+	ft_list_init(&listeners, listeners_on_remove);
+	ft_list_init(&streams, streams_on_remove);
 
 	// Prepare listening socket(s)
 	rc = listening_socket_create_getaddrinfo(on_resolve_listen_cb, NULL, &context, AF_INET, SOCK_STREAM, "127.0.0.1", "12345");
@@ -215,7 +210,7 @@ int main(int argc, char const *argv[])
 	ev_unref(context.ev_loop);
 
 	// Start listening
-	ft_list_each(&listen_socks, listen_sock_start_each, NULL);
+	ft_list_each(&listeners, listen_sock_start_each, NULL);
 
 	// Register exiting watcher
 	context_exiting_watcher_add(&context, &watcher, on_exiting_cb);
@@ -223,13 +218,9 @@ int main(int argc, char const *argv[])
 	// Enter event loop
 	context_evloop_run(&context);
 
-	// Remove established sockets
-	ft_list_fini(&established_socks);
-
-	// Remove listening sockets
-	ft_list_fini(&listen_socks);
-
-	// Finalize context
+	// Clean up
+	ft_list_fini(&streams);
+	ft_list_fini(&listeners);
 	context_fini(&context);
 
 	return EXIT_SUCCESS;
