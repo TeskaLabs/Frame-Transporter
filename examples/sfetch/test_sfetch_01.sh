ce20bd7732cf7fef17e02bcdcd11f02f7d634d15
Ales Teska <ales.teska@teskalabs.com>
2016-08-19 00:42:13 +0100

    Improving SSL sites test.

--- examples/sfetch/test_sfetch_01.sh
+++ examples/sfetch/test_sfetch_01.sh
@@ -1,13 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
 
 COUNTER_OK=0
 COUNTER_FAILED=0
 
 while read p; do
 	read -ra arr <<< "${p}"
-	set +e
 	./sfetch ${arr[0]} ${arr[1]} > /dev/null
-	set -e
 	if [ $? -ne 0 ]; then
 		COUNTER_FAILED=$[$COUNTER_FAILED +1]
 		echo ${p} "FAILED"
