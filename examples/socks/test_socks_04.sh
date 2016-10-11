79ae732d2b53902d13de90ac636416f048c569b3
Ales Teska <ales.teska@teskalabs.com>
2016-10-11 18:32:34 +0100

    And another one.

--- examples/socks/test_socks_04.sh
+++ examples/socks/test_socks_04.sh
@@ -6,26 +6,26 @@ function finish
 	if [[ !  -z  ${JOBS}  ]] ; then
 		echo ${JOBS} | xargs kill ;
 	fi
-	rm -f /tmp/sc-test-socks-01.bin /tmp/sc-test-socks-01o.bin /tmp/sc-test-socks-01.chsum
+	rm -f /tmp/sc-test-socks-04.bin /tmp/sc-test-socks-04o.bin /tmp/sc-test-socks-04.chsum
 }
 trap finish EXIT
 
 echo "Testing SOCKS5 on IPv6"
 
 ./socks &
-nc -d -l ::1 12347 >/tmp/sc-test-socks-01o.bin &
+nc -d -l ::1 12347 >/tmp/sc-test-socks-04o.bin &
 NC_PID=$!
 
-dd if=/dev/urandom count=10240 bs=1024 > /tmp/sc-test-socks-01.bin
+dd if=/dev/urandom count=10240 bs=1024 > /tmp/sc-test-socks-04.bin
 
-cat /tmp/sc-test-socks-01.bin | pv -r | nc -X5 -x 127.0.0.1:12345 ::1 12347
+cat /tmp/sc-test-socks-04.bin | pv -r | nc -X5 -x 127.0.0.1:12345 ::1 12347
 
 wait ${NC_PID}
 
-ls -l  /tmp/sc-test-socks-01.bin /tmp/sc-test-socks-01o.bin
-shasum /tmp/sc-test-socks-01.bin /tmp/sc-test-socks-01o.bin
+ls -l  /tmp/sc-test-socks-04.bin /tmp/sc-test-socks-04o.bin
+shasum /tmp/sc-test-socks-04.bin /tmp/sc-test-socks-04o.bin
 
-cat /tmp/sc-test-socks-01.bin | shasum  > /tmp/sc-test-socks-01.chsum
-cat /tmp/sc-test-socks-01o.bin | shasum --check /tmp/sc-test-socks-01.chsum
+cat /tmp/sc-test-socks-04.bin | shasum  > /tmp/sc-test-socks-04.chsum
+cat /tmp/sc-test-socks-04o.bin | shasum --check /tmp/sc-test-socks-04.chsum
 
 echo "TEST $0 OK"
